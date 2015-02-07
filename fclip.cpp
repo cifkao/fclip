febef3ae3087a0a7c17e101580e20bbb6d2ed244
Ondrej Cifka <ondra@cifka.com>
2015-02-07 16:35:26 +0100

    Change option parsing; add foreach command stub

--- fclip.cpp
+++ fclip.cpp
@@ -19,12 +19,29 @@ bool oVerbose;
 bool oNonRecursive;
 // "remove" options:
 bool oRemoveParents;
+// "for-each" options:
+bool oForEachRemove;
 
 
 DBus::BusDispatcher dispatcher;
 
 vector<string> serverMessages;
 
+
+string expandCommand(string cmd){
+  if(cmd == "rm") return "remove";
+  if(cmd == "ls") return "list";
+  if(cmd == "each" || cmd == "foreach") return "for-each";
+  return cmd;
+}
+
+po::options_description general_options(){
+  po::options_description options("Options");
+  options.add_options()
+          ("help,h", "display help and exit")
+          ("verbose,v", po::bool_switch(&oVerbose), "explain what is being done");
+  return options;
+}
 
 po::options_description help_options(){
   return po::options_description();
@@ -48,7 +65,11 @@ po::options_description list_options(){
   return po::options_description();
 }
 
-bool help_run(const vector<string> &argv){
+po::options_description forEach_options(){
+  return po::options_description();
+}
+
+void help_run(const vector<string> &argv){
   po::options_description options = help_options();
   options.add_options()
           ("command", po::value<string>());
@@ -63,24 +84,39 @@ bool help_run(const vector<string> &argv){
   po::notify(vm);
   
   if(vm.count("command")){
-    const string &cmd = vm["command"].as<string>();
+    const string &cmd = expandCommand(vm["command"].as<string>());
     if(cmd == "help"){
       cout << "usage: fclip help [<command>]" << endl << endl;
-      cout << "Displays help information about a command." << endl;
+      cout << "Display help information about a command." << endl;
       cout << help_options() << endl;
     }else if(cmd == "add"){
       cout << "usage: fclip add [<options>] <files>..." << endl << endl;
-      cout << "Adds a file to the clipboard." << endl << endl;
+      cout << "Add one or more files to the clipboard." << endl << endl;
       cout << add_options() << endl;
     }else if(cmd == "remove"){
       cout << "usage: fclip remove [<options>] <files>..." << endl;
-      cout << "alias: rm" << endl;
-      cout << "Remove a file from the clipboard." << endl << endl;
-      cout << add_options() << endl;
-    }else return false;
-    return true;
+      cout << "alias: rm" << endl << endl;
+      cout << "Remove one or more files from the clipboard." << endl << endl;
+      cout << remove_options() << endl;
+    }else if(cmd == "for-each"){
+      cout << "usage: fclip for-each <commands>..." << endl;
+      cout << "aliases: foreach, each" << endl << endl;
+      cout << "Perform actions for each file in the clipboard." << endl << endl;
+      cout << forEach_options() << endl;
+    }else goto generalHelp;
+    return;
   }
-  return false;
+  
+  generalHelp:
+  cout << "usage: fclip [<options>] <command> [<command-options>]" << endl << endl;
+  cout << "Commands:" << endl;
+  cout << left;
+  cout << setw(24) << "  add " << "add files to the clipboard" << endl;
+  cout << setw(24) << "  foreach, each " << "perform actions for each file in the clipboard" << endl;
+  cout << setw(24) << "  help " << "get help about a command" << endl;
+  cout << setw(24) << "  remove, rm " << "remove files from the clipboard" << endl;
+  cout << endl;
+  cout << general_options() << endl;
 }
 
 
@@ -198,68 +234,93 @@ bool list_run(const vector<string> &argv, FclipClient &fclip){
   }else return false;
 }
 
-string expandCommand(string cmd){
-  if(cmd == "rm") return "remove";
-  if(cmd == "ls") return "list";
-  return cmd;
+bool forEach_run(const vector<string> &argv, FclipClient &fclip){
+  po::options_description options = list_options();
+  options.add_options()
+          ("command", po::value<vector<string> >());
+  po::positional_options_description positional;
+  positional.add("command", -1);
+  po::variables_map vm;
+  po::store(po::command_line_parser(argv)
+            .options(options)
+            .positional(positional)
+            .run(), vm);
+  po::notify(vm);
+  
+  string fifoPath;
+  {
+    namespace fs = boost::filesystem;
+    fs::path fifoPath_ = fs::temp_directory_path() / fs::unique_path();
+    fifoPath = fifoPath_.string();
+  }
+  
+  
+  
+  if(vm.count("command")){
+    vector<string> commands = vm["command"].as<vector<string> >();
+    for(auto cmd : commands){
+      system(cmd.c_str());
+    }
+  }
+  
+  return true;
 }
 
 int main(int argc, char** argv) {
-  po::options_description options("Options");
-  options.add_options()
-          ("help,h", "display help and exit")
-          ("verbose,v", po::bool_switch(&oVerbose), "explain what is being done");
-  
-  po::variables_map vm;
-  po::parsed_options parsed = po::command_line_parser(argc, argv)
-            .options(options)
-            .allow_unregistered()
-            .run();
-  po::store(parsed, vm);
-  po::notify(vm);
-  
-  vector<string> subargs = po::collect_unrecognized(parsed.options, po::include_positional);
-  
-  if(vm.count("help") || subargs.empty()) {
-    if(subargs.empty()){
-      cout << options << endl;
-      return EXIT_FAILURE;
-    }else{
-      subargs.insert(subargs.begin(), "help");
-    }
-  }
-  
-  string cmd = expandCommand(subargs[0]);
-  subargs.erase(subargs.begin());
-  
-  if(cmd == "help"){
-    if(!help_run(subargs))
-      cout << options << endl;
-    return EXIT_FAILURE;
-  }
-  
   bool success = true;
+  
   try{
-    DBus::default_dispatcher = &dispatcher;
-    DBus::Connection bus = DBus::Connection::SessionBus();
-    FclipClient fclip(bus);
-    
-    if(cmd == "add"){
-      success = success && add_run(subargs, fclip);
-    }else if(cmd == "remove"){
-      success = success && remove_run(subargs, fclip);
-    }else if(cmd == "list"){
-      success = success && list_run(subargs, fclip);
-    }else{
-      throw runtime_error("unrecognised command or option '" + cmd + "'");
+    po::options_description options = general_options();
+    po::variables_map vm;
+    po::parsed_options parsed = po::command_line_parser(argc, argv)
+              .options(options)
+              .allow_unregistered()
+              .run();
+    po::store(parsed, vm);
+    po::notify(vm);
+  
+    vector<string> subargs = po::collect_unrecognized(parsed.options, po::include_positional);
+
+    if(vm.count("help") || subargs.empty()) {
+      subargs.insert(subargs.begin(), "help");
+      if(subargs.empty()){
+        help_run(subargs);
+        return EXIT_FAILURE;
+      }
     }
-    
-    for(const string &str : serverMessages){
-      cerr << str << endl;
+
+    string cmd = expandCommand(subargs[0]);
+    subargs.erase(subargs.begin());
+
+    if(cmd == "help"){
+      help_run(subargs);
+      return EXIT_FAILURE;
     }
-  }catch(const DBus::Error &e){
-    cerr << "failed to connect to fclip server" << endl;
-    return EXIT_FAILURE;
+
+    try{
+      DBus::default_dispatcher = &dispatcher;
+      DBus::Connection bus = DBus::Connection::SessionBus();
+      FclipClient fclip(bus);
+
+      if(cmd == "add"){
+        success = success && add_run(subargs, fclip);
+      }else if(cmd == "remove"){
+        success = success && remove_run(subargs, fclip);
+      }else if(cmd == "list"){
+        success = success && list_run(subargs, fclip);
+      }else if(cmd == "for-each"){
+        success = success && forEach_run(subargs, fclip);
+      }else{
+        throw runtime_error("unrecognised command or option '" + cmd + "'");
+      }
+
+      for(const string &str : serverMessages){
+        cerr << str << endl;
+      }
+    }catch(const DBus::Error &e){
+      cerr << "failed to connect to fclip server" << endl;
+      return EXIT_FAILURE;
+    }
   }catch(const exception &e){
     cerr << e.what() << endl;
     return EXIT_FAILURE;
